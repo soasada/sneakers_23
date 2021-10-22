15c52f35a7e15068644db6cc515d067eb48cf2f4
Nicolas Vargas Ortega <nvortega92@gmail.com>
2021-10-22 15:09:05 +0200

    Add cart id generation plug to have it in every page available

--- lib/sneakers_23/checkout.ex
+++ lib/sneakers_23/checkout.ex
@@ -1,6 +1,8 @@
 defmodule Sneakers23.Checkout do
   alias Sneakers23.Checkout.ShoppingCart
 
+  @cart_id_length 64
+
   defdelegate add_item_to_cart(cart, item), to: ShoppingCart, as: :add_item
   defdelegate remove_item_from_cart(cart, item), to: ShoppingCart, as: :remove_item
   defdelegate cart_item_ids(cart), to: ShoppingCart, as: :item_ids
@@ -13,5 +15,11 @@ defmodule Sneakers23.Checkout do
       {:ok, cart} -> cart
       {:error, _} -> restore_cart(nil)
     end
+  end
+
+  def generate_cart_id() do
+    :crypto.strong_rand_bytes(@cart_id_length)
+    |> Base.encode64()
+    |> binary_part(0, @cart_id_length)
   end
 end
